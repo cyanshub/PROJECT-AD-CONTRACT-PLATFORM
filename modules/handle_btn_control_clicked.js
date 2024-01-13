7b667e4d262faf549e0f142bb99f2c6aafa9b2b0
travel15740 <travel15740@gmail.com>
2024-01-13 22:54:50 +0800

    表單 form 樣式設計

--- modules/handle_btn_control_clicked.js
+++ modules/handle_btn_control_clicked.js
@@ -1,19 +1,34 @@
-// 載入封包函式
-import {setBtnDisabled} from "./set_btn_disabled.js"
-
 // 進行 DOM 操作選取物件
 const form = document.getElementById('a-form')
 const formParts = form.querySelectorAll('.part')
 const stepControl = document.getElementById('step-control')
 const steps = stepControl.querySelectorAll('.step')
+const btnControl = document.getElementById('btn-control')
+const nextBtn = btnControl.querySelector('.btn-primary')
+const prevBtn = btnControl.querySelector('.btn-outline')
 
 // 定義變數
 let step = 0
 
+// 定義按鈕失效互動
+function setBtnDisabled(step) {
+  if (step === 0) {
+    prevBtn.setAttribute('disabled', 'disabled')
+  } else {
+    prevBtn.removeAttribute('disabled')
+  }
+  if (step === 2) {
+    nextBtn.innerHTML = '送出申請'
+  } else {
+    nextBtn.innerHTML = '下一步'
+  }
+}
+
+
 export function handleBtnControlClicked(e) {
   e.preventDefault()
   const nowStep = steps[step]
-  if (e.target.matches('.btn-primary') && e.target.innerHTML === '下一步') {
+  if (e.target.matches('.btn-primary')) {
     const nextStep = steps[step + 1]
     nowStep.classList.remove('active')
     nowStep.classList.add('checked')
@@ -24,11 +39,14 @@ export function handleBtnControlClicked(e) {
   } else if (e.target.matches('.btn-outline')) {
     const prevStep = steps[step - 1]
     nowStep.classList.remove('active')
+    nowStep.classList.remove('checked')
     prevStep.classList.remove('checked')
     prevStep.classList.add('active')
     formParts[step].classList.toggle('d-none')
     formParts[step - 1].classList.toggle('d-none')
     step -= 1
   }
-  setBtnDisabled()
+  setBtnDisabled(step)
 }
+
+
